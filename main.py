b9088a3e7aba4bd8364c70ac190e25c5373c5c03
yugant10-commits <yugantghimire12345@gmail.com>
2024-02-02 17:30:21 +0545

    Added: frontend for the chat web

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from src.scrapper import ScrapeWebPage
 from src.compressed_search import SimilarityCalculator
 from src.vector_search import VectorSearch
-
+import streamlit as st
 if __name__ == '__main__':
-    query="give me more information about sharad rai"
-    url = "https://tai.com.np/"
-    url_scrapper = ScrapeWebPage(url)
-    url_list = url_scrapper.get_url()
-    content = url_scrapper.get_page_contents(url_list = url_list)
-    # print(content)
-    # obj = SimilarityCalculator(context_list=content, query=query)
-    # final_compute = obj._compute_distance()
-    # results = obj.get_k_closest_result(k=2)
-    # print(results)
-    # print(results)
-    vector_obj = VectorSearch(data=content, model_name="all-MiniLM-L6-v2")
-    docs, metadatas = vector_obj._split_data()
-    data_store = vector_obj._faiss_search()
-    result = data_store.similarity_search(query)
-    print(result)
+    st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
+    # st.sidebar.image("img/ai.png")
+    st.header("`Interweb Explorer`")
+    st.info("`I am an AI that can answer questions by exploring, reading, and summarizing web pages."
+        "I can be configured to use different modes: public API or private (no data sharing).`")
+
+    # User input 
+    url = st.text_input("`Please add a URL: `")
+    question = st.text_input("`Ask a question:`")
+    
+    if url:
+        url_scrapper = ScrapeWebPage(url)
+        url_list = url_scrapper.get_url()
+        content = url_scrapper.get_page_contents(url_list = url_list)
+        vector_obj = VectorSearch(data=content, model_name="all-MiniLM-L6-v2")
+
+    if question:
+        # Generate answer (w/ citations)
+        # Write answer and sources
+        answer = st.empty()
+        docs, metadatas = vector_obj._split_data()
+        data_store = vector_obj._faiss_search()
+        result = data_store.similarity_search(question)
+        st.write(result)
+        # answer.info('`Answer:`\n\n' + result)
+        # st.info('`Sources:`\n\n' + result['sources'])
+    # print(result)
     
     
     
